0ecf72691b4c4e33c764501ac1aea12dc314109d
Geoffrey Challen <challen@illinois.edu>
2020-05-19 08:40:14 -0500

    Add sidebar to menu.

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,18 +1,36 @@
-import React from "react"
+import React, { useRef } from "react"
 import PropTypes from "prop-types"
 
-import { Container } from "semantic-ui-react"
+import { Container, Ref, Segment, Responsive, Rail, Sticky } from "semantic-ui-react"
 import { MDXProvider } from "@mdx-js/react"
 
-import { Highlighted } from "@cs125/semantic-ui"
+import { Highlighted, headings, SidebarMenu } from "@cs125/semantic-ui"
 const components = {
   code: Highlighted,
+  ...headings,
 }
-const Layout: React.FC = ({ children }) => (
-  <Container text style={{ paddingTop: 16 }}>
-    <MDXProvider components={components}>{children}</MDXProvider>
-  </Container>
-)
+
+const Layout: React.FC = ({ children }) => {
+  const contextRef = useRef()
+  return (
+    <Container text style={{ paddingTop: 16, paddingBottom: 16 }}>
+      <Ref innerRef={contextRef}>
+        <Segment basic>
+          <Responsive minWidth={1200}>
+            <Rail position="right">
+              <Sticky context={contextRef}>
+                <Segment basic style={{ paddingTop: 64 }}>
+                  <SidebarMenu />
+                </Segment>
+              </Sticky>
+            </Rail>
+          </Responsive>
+          <MDXProvider components={components}>{children}</MDXProvider>
+        </Segment>
+      </Ref>
+    </Container>
+  )
+}
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
