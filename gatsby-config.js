8ff514d5d0ea80c00d65b5f446185a20fbf87ab3
Geoffrey Challen <challen@illinois.edu>
2020-05-19 14:40:05 -0500

    Add logo and metadata.

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,5 +25,21 @@ module.exports = {
       },
     },
     "gatsby-plugin-preload-fonts",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: process.env.npm_package_name,
+        short_name: process.env.npm_package_name,
+        start_url: "/",
+        icon: "src/images/favicon.png",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-env-variables",
+      options: {
+        whitelist: ["npm_package_name", "npm_package_version", "npm_package_description"],
+      },
+    },
   ],
 }
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,8 +1,11 @@
 import React, { useRef } from "react"
 import PropTypes from "prop-types"
 
-import { Container, Ref, Segment, Responsive, Rail, Sticky } from "semantic-ui-react"
+import { Helmet } from "react-helmet"
+import { Container, Ref, Segment, Responsive, Rail, Sticky, Image } from "semantic-ui-react"
 import { MDXProvider } from "@mdx-js/react"
+
+import logo from "../images/favicon.png"
 
 import { Highlighted, headings, SidebarMenu } from "@cs125/semantic-ui"
 const components = {
@@ -13,22 +16,33 @@ const components = {
 const Layout: React.FC = ({ children }) => {
   const contextRef = useRef()
   return (
-    <Container text style={{ paddingTop: 16, paddingBottom: 16 }}>
-      <Ref innerRef={contextRef}>
-        <Segment basic>
-          <Responsive minWidth={1200}>
-            <Rail position="right">
-              <Sticky context={contextRef}>
-                <Segment basic style={{ paddingTop: 64 }}>
-                  <SidebarMenu />
-                </Segment>
-              </Sticky>
-            </Rail>
-          </Responsive>
-          <MDXProvider components={components}>{children}</MDXProvider>
-        </Segment>
-      </Ref>
-    </Container>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{process.env.npm_package_name}</title>
+        <meta name="description" content={process.env.npm_package_description} />
+        <link rel="preconnect" href="https://accounts.google.com" crossOrigin="use-credentials" />
+        <link rel="preconnect" href="https://ssl.gstatic.com" crossOrigin="use-credentials" />
+        <link rel="preconnect" href="https://apis.google.com" crossOrigin="use-credentials" />
+      </Helmet>
+      <Container text>
+        <Ref innerRef={contextRef}>
+          <Segment basic>
+            <Responsive minWidth={1200}>
+              <Rail position="right">
+                <Sticky context={contextRef}>
+                  <Segment basic style={{ paddingTop: 64 }}>
+                    <SidebarMenu />
+                  </Segment>
+                </Sticky>
+              </Rail>
+            </Responsive>
+            <Image floated={"right"} src={logo} style={{ width: "4rem" }} href={"https://cs125.cs.illinois.edu"} />
+            <MDXProvider components={components}>{children}</MDXProvider>
+          </Segment>
+        </Ref>
+      </Container>
+    </>
   )
 }
 Layout.propTypes = {
